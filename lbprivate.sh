f6c71a4980ed9a9de4abe0818100883faecfced9
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2019-06-12 07:46:37 -0700

    Update lbprivate.sh

--- lbprivate.sh
+++ lbprivate.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-      apt-get update
-      apt-get install apache2 -y
-      sed -i '/Listen 80/c\Listen 110' /etc/apache2/ports.conf
-      sed -i '/\/c\\' /etc/apache2/sites-enabled/000-default.conf
-      service apache2 restart
-      echo '<!doctype html><html><body><h1>lbs1priv</h1></body></html>' | tee /var/www/html/index.html
+apt-get update
+apt-get install apache2 -y
+sed -i '/Listen 80/c\Listen 110' /etc/apache2/ports.conf
+sed -i '/\/c\\' /etc/apache2/sites-enabled/000-default.conf
+service apache2 restart
+echo '<!doctype html><html><body><h1>lbs1priv</h1></body></html>' | tee /var/www/html/index.html
