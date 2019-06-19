44eec1ac3a315967adc4dc5e5b800ce0f126250a
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2019-06-19 12:36:31 -0700

    Update lbpublic.sh

--- lbpublic.sh
+++ lbpublic.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-apt-get update -y
+apt-get update
 apt-get install apache2 -y
+sed -i '/Listen 80/c\Listen 8000' /etc/apache2/ports.conf
+sed -i '/\/c\\' /etc/apache2/sites-enabled/000-default.conf
 service apache2 restart
-echo '<!doctype html><html><body><h1>lbs2publ</h1></body></html>' | tee /var/www/html/index.html
+echo '<!doctype html><html><body><h1>lbpubli2</h1></body></html>' | tee /var/www/html/index.html
