3b6f59f783516e894d712ffd7f61ecc9d5322050
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2019-06-19 14:23:01 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2-binary',
         'NAME': 'myproject',
         'USER': 'myprojectuser',
         'PASSWORD': 'password',
